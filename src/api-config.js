b5e60c5f61c42a79a9d4cfbea61c564a12e85eb6
wonsy1006 <wonsy1006@gmail.com>
2022-12-12 10:11:08 +0900

    commit for deployment

---
+++ src/api-config.js
@@ -0,0 +1,11 @@
+let backendHost;
+
+const hostname = window && window.location && window.location.hostname;
+
+if (hostname === 'localhost') {
+  backendHost = 'http://localhost:8080';
+} else {
+  // backendHost = '백엔드 일래스틱 빈스톡 주소'
+}
+
+export const API_BASE_URL = `${backendHost}`;
