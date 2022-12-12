ba7dc759f1a065afe4d6338cef28c45a4f979c5e
wonsy1006 <wonsy1006@gmail.com>
2022-12-12 12:14:14 +0900

    invalid host header 오류 해결 중

--- src/api-config.js
+++ src/api-config.js
@@ -1,11 +1,11 @@
-let backendHost;
+// let backendHost;
 
-const hostname = window && window.location && window.location.hostname;
+// const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'localhost') {
-  backendHost = 'http://localhost:8080';
-} else {
-  // backendHost = '백엔드 일래스틱 빈스톡 주소'
-}
+// if (hostname === 'localhost') {
+//   backendHost = 'http://localhost:8080';
+// } else {
+//   // backendHost = '백엔드 일래스틱 빈스톡 주소'
+// }
 
-export const API_BASE_URL = `${backendHost}`;
+// export const API_BASE_URL = `${backendHost}`;
