b579144c90b713bf2a5ca02c43793f0fd61d70f3
wonsy1006 <wonsy1006@gmail.com>
2022-12-28 20:01:41 +0900

    axios instance 생성

---
+++ src/app/module/instance.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  headers: {},
+  baseURL: '',
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('accessToken');
+
+  if (!token) {
+    config.headers['accessToken'] = null;
+    config.headers['refreshToken'] = null;
+    return config;
+  }
+  if (config.headers && token) {
+    const { accessToken, refreshToken } = JSON.parse(token);
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+    config.headers['refreshToken'] = `Bearer ${refreshToken}`;
+    return config;
+  }
+});
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 401) {
+      const refreshToken = originalConfig.headers['refreshToken'];
+      try {
+        const data = await axios({
+          url: `/auth/reissue`,
+          method: 'GET',
+          headers: {
+            Authorization: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.setItem(
+            'accessToken',
+            JSON.stringify(data.data, ['accessToken', 'refreshToken']),
+          );
+          return await instance.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('Token reissue error');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
+export default instance;
