abe7a9791ed23f9b0a43ac4132888571dafd3825
wonsy1006 <wonsy1006@gmail.com>
2022-12-30 01:31:04 +0900

    feed 수정

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -11,18 +11,16 @@ const Feed = () => {
   }, [dispatch]);
 
   const { dajim } = useSelector((state) => state.dajim);
+  console.log(dajim);
 
   return (
     <>
       <h2>오늘의 다짐</h2>
-      {dajim.map((dj) => {
+      {/* {dajim.map((dj) => {
         return (
           <FeedItem key={dj.id} user="user1" dajimContent={dj.dajimContent} />
         );
-      })}
-      {/* <FeedItem user="user1" dajimContent="깃허브 커밋하기" />
-      <FeedItem user="user2" dajimContent="퇴근하면 바로 운동 가기" />
-      <FeedItem user="user3" dajimContent="요미 산책시키기" /> */}
+      })} */}
     </>
   );
 };
