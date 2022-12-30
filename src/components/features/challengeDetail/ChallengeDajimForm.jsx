7cec16de183d42ea120bda105635ac6d7f1b28fd
wonsy1006 <wonsy1006@gmail.com>
2022-12-30 17:10:38 +0900

    다짐 폼 수정

--- src/components/features/challengeDetail/ChallengeDajimForm.jsx
+++ src/components/features/challengeDetail/ChallengeDajimForm.jsx
@@ -16,6 +16,8 @@ const ChallengeDajimForm = ({ getBackToEditMode }) => {
   const params = useParams();
   const challengeId = parseInt(params.roomNumber);
 
+  const { dajim } = useSelector((state) => state.dajim);
+
   const submitForm = (data) => {
     const open = data.open;
     data.open = open.toUpperCase();
@@ -34,7 +36,7 @@ const ChallengeDajimForm = ({ getBackToEditMode }) => {
         <StyledInput
           {...register('dajimNumber', { valueAsNumber: true })}
           type="number"
-          defaultValue=""
+          defaultValue={dajim.dajimNumber === '' ? '' : dajim.dajimNumber}
         />
         <StyledInput
           {...register('challengeId', { valueAsNumber: true })}
