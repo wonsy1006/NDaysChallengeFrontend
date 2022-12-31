d2e15ce9bff21def6a1c297cff9283a5f2503600
wonsy1006 <wonsy1006@gmail.com>
2022-12-31 18:21:41 +0900

    profilePic import 수정

--- src/components/common/ProfilePic.jsx
+++ src/components/common/ProfilePic.jsx
@@ -8,19 +8,19 @@ import { ReactComponent as Pic6 } from '../../assets/images/profile_pics/pic6.sv
 
 const ProfilePic = props => {
   const picType = props.picType;
-  switch (props.picType) {
+  switch (picType) {
     case 'pic1':
-      return <Pic1 />;
+      return <img src={Pic1} />;
     case 'pic2':
-      return <Pic2 />;
+      return <img src={Pic2} />;
     case 'pic3':
-      return <Pic3 />;
+      return <img src={Pic3} />;
     case 'pic4':
-      return <Pic4 />;
+      return <img src={Pic4} />;
     case 'pic5':
-      return <Pic5 />;
+      return <img src={Pic5} />;
     case 'pic6':
-      return <Pic6 />;
+      return <img src={Pic6} />;
     default:
       return null;
   }
