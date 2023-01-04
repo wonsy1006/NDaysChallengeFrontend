0033dd5eb2e45f67305011d34789be62e851898e
wonsy1006 <wonsy1006@gmail.com>
2023-01-04 20:40:43 +0900

    이메일 기본 값 null로 지정

--- src/pages/FriendsList.jsx
+++ src/pages/FriendsList.jsx
@@ -64,7 +64,7 @@ const FriendsList = () => {
             margin="1.2rem auto 2.4rem"
           >
             <StyledInput {...register('nickname')} type="text" />
-            <StyledInput {...register('id')} type="email" />
+            <StyledInput {...register('id')} type="email" defaultValue={null} />
             <IconWrapper>
               <Button>검색</Button>
             </IconWrapper>
