fbe3dbc60a304ab113bf7948ef0b6b535931f90b
wonsy1006 <wonsy1006@gmail.com>
2023-01-10 19:02:06 +0900

    공유 기능 수정

--- src/components/features/invite/Invite.jsx
+++ src/components/features/invite/Invite.jsx
@@ -8,24 +8,24 @@ import {
   FacebookIcon,
 } from '../../common/Icon';
 
-const shareTwitter = () => {
-  var sendText = '엔챌에서 나만의 챌린지를 만들어 보세요!'; // 전달할 텍스트
-  var sendUrl = 'http://ndayschallenge.com'; // 전달할 URL
-  window.open(
-    'https://twitter.com/intent/tweet?text=' + sendText + '&url=' + sendUrl,
-  );
-};
-
-const shareKakaotalk = () => {};
-
-const shareInstagram = () => {};
-
-const shareFacebook = () => {
-  const sendUrl = 'http://ndayschallenge.com'; // 전달할 URL
-  window.open('http://www.facebook.com/sharer/sharer.php?u=' + sendUrl);
-};
-
 const Invite = () => {
+  const shareTwitter = () => {
+    var sendText = '엔챌에서 나만의 챌린지를 만들어 보세요!'; // 전달할 텍스트
+    var sendUrl = 'http://ndayschallenge.com'; // 전달할 URL
+    window.open(
+      'https://twitter.com/intent/tweet?text=' + sendText + '&url=' + sendUrl,
+    );
+  };
+
+  const shareKakaotalk = () => {};
+
+  const shareInstagram = () => {};
+
+  const shareFacebook = () => {
+    const sendUrl = 'http://ndayschallenge.com'; // 전달할 URL
+    window.open('http://www.facebook.com/sharer/sharer.php?u=' + sendUrl);
+  };
+
   return (
     <ColumnWrapper id="invite" margin="3.2rem 0">
       <h3>친구 초대하기</h3>
