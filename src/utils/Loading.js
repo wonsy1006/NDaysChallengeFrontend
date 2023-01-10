cb46d2e65bed9d17207709296b13b594b1c4ae88
wonsy1006 <wonsy1006@gmail.com>
2023-01-10 21:26:25 +0900

    Loading.js 스타일 수정

--- src/utils/Loading.js
+++ src/utils/Loading.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SyncLoader from 'react-spinners/SyncLoader';
 import styled from 'styled-components';
-import { ColumnWrapper, RowWrapper } from '../components/common/Wrapper';
+import { RowWrapper } from '../components/common/Wrapper';
 
 function Loading() {
   return (
     <LoaderWrapper>
-      <SyncLoader color="#0B62AA" height={15} width={5} />
+      <SyncLoader color="#0B62AA" />
     </LoaderWrapper>
   );
 }
@@ -17,5 +17,5 @@ const LoaderWrapper = styled(RowWrapper)`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  height: 100%;
 `;
