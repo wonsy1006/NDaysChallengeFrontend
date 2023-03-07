743b1043ae0651797edaee0a44c5dcbd842b7c19
wonsy1006 <wonsy1006@gmail.com>
2023-03-07 17:37:46 +0900

    interceptor 주석 처리 해제

--- src/app/module/instance.jsx
+++ src/app/module/instance.jsx
@@ -8,33 +8,33 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-// instance.interceptors.request.use((config) => {
-//   const [cookies] = useCookies(['accessToken']);
-//   const accessToken = cookies.accessToken;
-//   if (accessToken) {
-//     config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-//   return config;
-// });
+instance.interceptors.request.use((config) => {
+  const [cookies] = useCookies(['accessToken']);
+  const accessToken = cookies.accessToken;
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
-// instance.interceptors.response.use(
-//   (response) => {
-//     const accessToken = response.headers['accessToken'];
-//     if (accessToken) {
-//       const [cookies, setCookie] = useCookies(['accessToken']);
-//       setCookie('accessToken', accessToken, { path: '/' });
-//     }
-//     return response;
-//   },
-//   (error) => {
-//     const response = error.response;
-//     if (response && response.status === 401) {
-//       const [cookies, removeCookie] = useCookies(['accessToken']);
-//       removeCookie('accessToken', { path: '/' });
-//     }
+instance.interceptors.response.use(
+  (response) => {
+    const accessToken = response.headers['accessToken'];
+    if (accessToken) {
+      const [cookies, setCookie] = useCookies(['accessToken']);
+      setCookie('accessToken', accessToken, { path: '/' });
+    }
+    return response;
+  },
+  (error) => {
+    const response = error.response;
+    if (response && response.status === 401) {
+      const [cookies, removeCookie] = useCookies(['accessToken']);
+      removeCookie('accessToken', { path: '/' });
+    }
 
-//     return Promise.reject(error);
-//   },
-// );
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
