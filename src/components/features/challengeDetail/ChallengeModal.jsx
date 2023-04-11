47fa2b238f244f5b0c87ca2b2312ccddea276433
wonsy1006 <wonsy1006@gmail.com>
2023-04-11 14:09:29 +0900

    ChallengeModal useState import 추가

--- src/components/features/challengeDetail/ChallengeModal.jsx
+++ src/components/features/challengeDetail/ChallengeModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeModal } from '../../../app/module/modalSlice';
 import { updateStamp } from '../../../app/module/challengeSlice';
