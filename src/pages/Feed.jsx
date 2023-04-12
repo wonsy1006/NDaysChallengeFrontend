1f54d1d751b6e4d3bd3cecb09aa867fceffdfe80
wonsy1006 <wonsy1006@gmail.com>
2023-04-12 21:59:14 +0900

    피드 데이터 변수 수정

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -16,13 +16,13 @@ const Feed = () => {
   const { feed } = useSelector((state) => state.dajim);
   const nickname = useSelector((state) => state.user.nickname);
 
-  const pagination = ({ feed }) => {};
+  console.log(feed);
 
   return (
     <>
       <h2>오늘의 다짐</h2>
       {loading && <Loading />}
-      {feed.map((dj) => {
+      {/* {feed.map((dj) => {
         return (
           <FeedItem
             key={dj.dajimNumber}
@@ -32,8 +32,8 @@ const Feed = () => {
             dajimContent={dj.content}
           />
         );
-      })}
-      <Pagination />
+      })} */}
+      {/* <Pagination /> */}
     </>
   );
 };
