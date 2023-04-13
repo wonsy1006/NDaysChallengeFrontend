e3918afea54ad2509b1a4763ff223998fb712162
wonsy1006 <wonsy1006@gmail.com>
2023-04-13 12:02:57 +0900

    FeedItem useState import 추가

--- src/components/features/feed/FeedItem.jsx
+++ src/components/features/feed/FeedItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Card from '../../common/Card';
 import styled from 'styled-components';
